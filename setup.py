92e07ea66d04bf9aae31523757eff2ae308ce0be
matan <matan@nui.ai>
2025-10-31 11:45:40 +0200

    fix build failure over absl version mismatch, https://github.com/google-ai-edge/mediapipe/issues/6132 should clarify why this mismatch.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ from setuptools.command import build_ext
 from setuptools.command import build_py
 from setuptools.command import install
 
-__version__ = 'dev'
+__version__ = '0.10.26+nui'
 MP_DISABLE_GPU = os.environ.get('MEDIAPIPE_DISABLE_GPU') != '0'
 IS_WINDOWS = (platform.system() == 'Windows')
 IS_MAC = (platform.system() == 'Darwin')
